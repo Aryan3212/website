9cf1cbfb94da9f28ed872c8f104ab39270a78c54
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2023-12-31 13:11:18 +0600

    chore: changed formatting settings

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,42 +1,42 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Aryan Rahman",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Aryan Rahman: Building Reality",
-	// Meta property used as the default description meta property
-	description: "Blog and website for Aryan Rahman",
-	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-US",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_US",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
-	date: {
-		locale: "en-US",
-		options: {
-			day: "numeric",
-			month: "short",
-			year: "numeric",
-		},
-	},
-	webmentions: {
-		link: "https://webmention.io/aryanrahman.dev/webmention",
-	},
+  // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+  author: "Aryan Rahman",
+  // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+  title: "Aryan Rahman: Building Reality",
+  // Meta property used as the default description meta property
+  description: "Blog and website for Aryan Rahman",
+  // HTML lang property, found in src/layouts/Base.astro L:18
+  lang: "en-US",
+  // Meta property, found in src/components/BaseHead.astro L:42
+  ogLocale: "en_US",
+  // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+  date: {
+    locale: "en-US",
+    options: {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    },
+  },
+  webmentions: {
+    link: "https://webmention.io/aryanrahman.dev/webmention",
+  },
 };
 
 // Used to generate links in both the Header & Footer.
 export const menuLinks: Array<{ title: string; path: string }> = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about/",
-	},
-	{
-		title: "Blog",
-		path: "/posts/",
-	},
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about/",
+  },
+  {
+    title: "Blog",
+    path: "/posts/",
+  },
 ];
--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
 export function remarkReadingTime() {
-	return function (tree, { data }) {
-		const textOnPage = toString(tree);
-		const readingTime = getReadingTime(textOnPage);
-		data.astro.frontmatter.minutesRead = readingTime.text;
-	};
+  return function (tree: unknown, { data }: any) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
 }
