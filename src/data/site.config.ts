8b6c4a7c646de1c104df7db3fd9129649b62c5df
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2026-08-18 08:29:13 +0600

    chore: update site metadata and improve header styles
    - Changed site name and short name in the web manifest and site configuration to reflect new branding.
    - Updated header styles in the index page for better visibility and consistency, including adjustments to font sizes and section titles.

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'Aryan Rahman',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'Aryan Rahman: Building Reality',
+	title: 'Aryan Rahman: Software Engineer',
 	// Meta property used as the default description meta property
 	description:
 		'Blog and website for Aryan Rahman, full-stack software product engineer based in Bangladesh',
